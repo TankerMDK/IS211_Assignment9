c407b01549a57aed938be8b0744ac93361d18f19
TankerMDK <TankerMDK@Gmail.com>
2025-04-01 00:03:00 -0400

    This is wild. I couldn't get data from Yahoo or cbs. I'd need to use other tools. I'll be trying that in the future.

--- scraper_one.py
+++ scraper_one.py
@@ -1,3 +1,42 @@
+# scraped from https://en.wikipedia.org/wiki/List_of_World_Series_champions
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://en.wikipedia.org/wiki/List_of_World_Series_champions"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+
+# Need to find the rows first.
+table = soup.find("table", class_="wikitable") #
+rows = table.find_all("tr")
+
+champions = []
+
+for row in rows[1:]: # skips the header
+    columns = row.find_all(["td", "th"])
+    if len(columns) < 6:
+        continue # to skip the notes
+
+    year = columns[0].text.strip().split("[")[0]  # "[" Gets rid of the footnote
+    winner = columns[1].text.strip().split("(")[0]
+    series = columns[3].text.strip().split("[")[0]
+    loser = columns[4].text.strip().split("(")[0]
+
+    champions.append({
+        "year": year,
+        "winner": winner,
+        "series_result": series,
+        "loser": loser
+    })
+"""This is currently set to print 10 results"""
+print(json.dumps(champions[:10], indent=2, ensure_ascii=False)) # ensure_ascii if fixing a unicode escape display issue.
+                                                                # It works. But the json file editing needs work.
+
+# Saving the json file
+with open("world_series_champions.json", "w") as f:
+    json.dump(champions, f, indent=2, ensure_ascii=False)
 
 if __name__ == "__main__":
     print("Running scrapper one...")
--- scraper_two.py
+++ scraper_two.py
@@ -1,3 +1,51 @@
+# scraped from https://en.wikipedia.org/wiki/List_of_Dragon_Ball_episodes
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://en.wikipedia.org/wiki/List_of_Dragon_Ball_episodes"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+
+# Gets all the tables.
+tables = soup.find_all("table", class_="wikitable")
+
+# Pulling Season 1
+episode_table = tables[1]
+
+episodes = []
+
+rows = episode_table.find_all("tr")
+
+real_episode_number = 1
+for row in rows[1:]:
+    columns = row.find_all("td")
+    if len(columns) != 6:
+        continue
+
+    full_title = columns[0].text.strip()
+
+    parts = full_title.split("Transliteration:")
+    english_title = parts[0].strip().strip('"')
+    japanese_title = parts[1].strip() if len(parts) > 1 else ""
+
+    episodes.append({
+        "episode": real_episode_number,
+        "english_title": english_title,
+        "japanese_title": japanese_title
+    })
+
+    real_episode_number += 1
+
+# This code is working with the season 1 table at 13 episode.*
+# Display sample in terminal
+for ep in episodes[:13]:
+    print(f"Episode {ep['episode']}: {ep['english_title']} / {ep['japanese_title']}")
+
+# Save to JSON
+with open("dragon_ball_episodes.json", "w", encoding="utf-8") as f:
+    json.dump(episodes, f, indent=2, ensure_ascii=False)
 
 if __name__ == "__main__":
     print("Running scrapper two...")
